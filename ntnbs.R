a32e8dabed00066b04ae1496cf0ad827783f8575
AugustoCL <augustoleal72@gmail.com>
2020-05-16 01:07:54 -0300

    cleaning code

--- ntnbs.R
+++ ntnbs.R
@@ -1,16 +1,15 @@
 # install required packages
 install.packages("GetTDData", "tidyverse", "extrafont", "ggrepel", "ggthemes") # run if you dont have installed before
 
-# load tidyverse and font packages
+# load packages and font
 library(tidyverse)
-library(ggthemes)
+library(GetTDData)
 library(extrafont)
 font_import()
 loadfonts(device = "win")
 custom_font <- "Gadugi"
 
-# load API and download data 
-library(GetTDData)
+# download data 
 download.TD.data(asset.codes = "NTN-B") 
 NTNB <- read.TD.files(asset.codes = "NTN-B")
 
@@ -19,25 +18,24 @@ venc_ntnbs <- as.Date(c("2020-08-15", "2024-08-15", "2035-05-15", "2050-08-15"))
 
 # wrangle the data
 NTNB %>% 
-  # filter bonds by maturity
-  filter(matur.date %in% venc_ntnbs, 
-         # filter by date of negociation
-         ref.date > as.Date("2019-08-01"), 
-         # exclude zero coupons bonds 
-         !str_detect(asset.code,"Principal")) %>%
+  filter(matur.date %in% venc_ntnbs, # filter bonds by maturity 
+         ref.date > as.Date("2019-08-01"), # filter by date of negociation
+         !str_detect(asset.code,"Principal")) %>% # exclude zero coupons bonds 
   mutate(taxa = yield.bid*100,
          NTN_B = format(matur.date, "%Y/%m"),
          label = if_else(ref.date == min(ref.date),
                          format(matur.date, "%Y"),
                          NA_character_)) %>%
   arrange(NTN_B,desc(ref.date), .by_group = TRUE) %>%
-  select(-c(yield.bid, asset.code, price.bid)) %>%
+  select(-c(yield.bid, asset.code, price.bid)) %>% 
   
   # configure the plot
   ggplot(aes(x = ref.date,y = taxa, color = NTN_B)) +
   geom_line(size = 1.5) +
-  geom_vline(xintercept = as.Date("2020-03-10"), linetype = "dashed", color = "darkgrey", size = .5) +
-  geom_vline(xintercept = as.Date("2020-04-23"), linetype = "dashed", color = "darkgrey", size = .5) +
+  geom_vline(xintercept = as.Date("2020-03-10"), 
+             linetype = "dashed", color = "darkgrey", size = .5) +
+  geom_vline(xintercept = as.Date("2020-04-23"), 
+             linetype = "dashed", color = "darkgrey", size = .5) +
   geom_label(aes(x = as.Date("2020-03-15"), y = 1, label = "Moro Day"), 
              hjust = 0, 
              vjust = 0.5, 
@@ -46,7 +44,8 @@ NTNB %>%
              label.size = .25, 
              family= custom_font, 
              size = 3) + 
-  geom_curve(aes(x = as.Date("2020-04-01"), y = 1.2, xend = as.Date("2020-04-21"), yend = 1.7), 
+  geom_curve(aes(x = as.Date("2020-04-01"), y = 1.2, 
+                 xend = as.Date("2020-04-21"), yend = 1.7), 
              colour = "#555555", 
              size= 0.6, 
              curvature = -0.3,
@@ -59,13 +58,13 @@ NTNB %>%
              label.size = .25, 
              family= "Gadugi", 
              size = 3) + 
-  geom_curve(aes(x = as.Date("2020-02-11"), y = 5.2, xend = as.Date("2020-03-08"), yend = 5.7), 
+  geom_curve(aes(x = as.Date("2020-02-11"), y = 5.2, 
+                 xend = as.Date("2020-03-08"), yend = 5.7), 
              colour = "#555555", 
              size= 0.6, 
              curvature = -0.4,
              arrow = arrow(length = unit(0.02, "npc"))) +
   ggrepel::geom_label_repel(aes(label = label),
-                   # nudge_x = -1,
                    na.rm = TRUE,
                    inherit.aes = TRUE,
                    family = custom_font) +
@@ -81,8 +80,7 @@ NTNB %>%
         plot.title =  element_text(hjust = 0.5),
         plot.background = element_rect(color = "white")) +
   
-  # save de plot
+  # save the plot
   ggsave(filename = "ntnb_plot.png",width = 6.4, height = 4.8)
 
-# code created by @AugustoCL
-#####
+##### code created by @AugustoCL
